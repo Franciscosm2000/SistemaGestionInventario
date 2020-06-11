a8ed7140721e514c16d32dc7dffc64e9d43cc24a
francisco <franciscosandoval2000@gmail.com>
2020-06-11 10:33:50 -0500

    Modificacion en soluciones

---
+++ SistemaInventario/Model/Coneccion/coneccionSQL.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+namespace Model.Coneccion
+{
+    class coneccionSQL
+    {
+        abstract class ConnectionToSQL
+        {
+            //String solo lectura 
+            private readonly string connectionString;
+
+            //Constructor
+            public ConnectionToSQL()
+            {
+                //Cadena servidor 
+                connectionString = @"server=tcp:FRANCISCO,12500; DataBase =PawnSystem; User Id = CasaDeEmpeño; Password=1320022077";
+                //Cadena de Coneccion Fran
+                // connectionString = "server = FRANCISCO; Database = PawnSystem ; User Id =sa; Password = 1320022077";
+                //Cadena de coneccion Saul
+                //Cadena de coneccion Dylan
+            }
+
+            //Solo accedido de una clase eradada
+            protected SqlConnection GetConnection()
+            {
+                return new SqlConnection(connectionString);
+            }
+        }
+}
