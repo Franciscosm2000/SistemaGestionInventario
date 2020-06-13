cd1730c52a0f8621bc1f9d3f095532c54116adf7
sauliño <saulantonio0317@gmail.com>
2020-06-12 22:52:18 -0600

    modelo PQ terminado

--- SQL/Modelo PQ.sql
+++ SQL/Modelo PQ.sql
@@ -1,29 +1,64 @@
-use sistemaInventario;
+ use sistemaInventario;
 
-/*modelo PQ*/
+ /*creacion para mi modelo parte 1*/
+ /*manera consultas*/
 
-/*creacion para mi modelo parte 1*/
-
-create procedure modelo_parte_1
-as 
-/*
-declare @contador int
-set @contador = (select ROW_NUMBER() over(order by lm.cantidad_Elaborar desc) AS [Lote] from Lista_Material lm);
-*/
+/*mis datos P*/
+create procedure Modelo_p1
+as
 select
-lm.id_listaMateriale,
-lm.Padre as[Modelo],
-lm.cantidad_Elaborar as [Volumen Lote],
-sum(sum(lm.cantidad_Elaborar)) over (order by lm.id_listaMateriale asc) as [Total Acumulado]
-/*(COUNT(lm.cantidad_Elaborar)*100 / (select Count(cantidad_Elaborar) From Lista_Material)) as porcentaje*/
+lm.id_listaMateriale as [LOTE],
+lm.Padre as[Material],
+lm.cantidad_Elaborar as [Cantidad]
 from Lista_Material lm 
 group by lm.id_listaMateriale, lm.Padre, lm.cantidad_Elaborar
 order by lm.id_listaMateriale asc
 
-/*creacion de mi modelo parte 2*/
+/*Ejecutar el proceso*/
+execute Modelo_p1
 
-/*sumando los modelos*/
-    SELECT lm.Padre , SUM(lm.cantidad_Elaborar) AS Total
-    FROM Lista_Material lm
-    GROUP BY lm.Padre
-	
+/*mis datos Q*/
+create procedure Modelo_q1
+as
+select
+lm.id_listaMateriale as [LOTE],
+lm.Padre as[Material],
+lm.cantidad_Elaborar as [Cantidad],
+sum(sum(lm.cantidad_Elaborar)) over (order by lm.id_listaMateriale asc) as [Total Acumulado],
+(round((100 * count(lm.cantidad_Elaborar)/ sum(lm.cantidad_Elaborar)),2)) as [Porcentaje]
+from Lista_Material lm 
+group by lm.id_listaMateriale, lm.Padre, lm.cantidad_Elaborar
+order by lm.id_listaMateriale asc
+
+/*Ejecutar el proceso*/
+execute Modelo_q1
+
+/*modelo */
+
+/*creacion de mi modelo parte 2*/
+/*manera consultas*/
+
+/*mis datos P*/
+create procedure Modelo_p2
+as
+SELECT lm.Padre as [Material], 
+SUM(lm.cantidad_Elaborar) AS [Cantidad]
+FROM Lista_Material lm
+GROUP BY lm.Padre
+
+/*Ejecutar el proceso*/
+execute Modelo_p2
+
+/*mis datos Q*/
+create procedure Modelo_q2
+as
+SELECT lm.Padre as [Material], 
+SUM(lm.cantidad_Elaborar) AS [Cantidad],
+sum(sum(lm.cantidad_Elaborar)) over (order by lm.Padre asc) as [Total Acumulado],
+(round((100 * count(lm.cantidad_Elaborar)/ sum(lm.cantidad_Elaborar)),2)) as [Porcentaje]
+FROM Lista_Material lm
+GROUP BY lm.Padre
+
+/*Ejecutar el proceso*/
+execute Modelo_q2
+ 
