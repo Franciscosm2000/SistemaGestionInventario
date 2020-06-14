d9253549f7eb6a574409c572dbcbedc414bd5f77
sauliño <saulantonio0317@gmail.com>
2020-06-13 18:28:16 -0600

    agregando mi estructura PQ

---
+++ SistemaInventario/SistemaInventario/Model/ConexionSQL/Conexion.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaInventario.Model.ConexionSQL
+{
+    abstract class Conexion
+    {
+        private readonly string conexionstring;
+
+        public Conexion()
+        {
+            conexionstring = "server= SAULINO; Database = sistemaInventario ; User Id =sa; Password = Java2019";
+        }
+
+        protected SqlConnection GetConnection()
+        {
+            return new SqlConnection(conexionstring);
+        }
+    }
+}
